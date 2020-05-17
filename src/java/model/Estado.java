4c753854e9755e4daa3f7fa51f347d21b3942559
fer153 <fer.carvalho153@gmail.com>
2020-05-17 20:23:51 -0300

    Add model and controller to project

---
+++ src/java/model/Estado.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author fernanda
+ */
+public class Estado {
+
+    private String nome;
+    private String uf;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getUF() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+}
