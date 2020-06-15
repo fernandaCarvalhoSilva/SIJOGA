0a62948b319bc1c5c1ef67f7872324af533ad5a2
fer153 <fer.carvalho153@gmail.com>
2020-06-14 22:24:25 -0300

    adicionando o projeto sijoga

---
+++ src/java/model/Usuario.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author fernanda
+ */
+public class Usuario {
+    
+    private Integer codigoEndereco;
+    private Integer codigoUsuario;
+    private Integer codigoCidade;
+    private String sobrenome;
+    private String uf_estado;
+    private String telefone;
+    private String email;
+    private String senha;
+    private String nome;
+    private String cpf;
+
+    public Integer getCodigoEndereco() {
+        return codigoEndereco;
+    }
+
+    public void setCodigoEndereco(Integer codigoEndereco) {
+        this.codigoEndereco = codigoEndereco;
+    }
+
+    public Integer getCodigoUsuario() {
+        return codigoUsuario;
+    }
+
+    public void setCodigoUsuario(Integer codigoUsuario) {
+        this.codigoUsuario = codigoUsuario;
+    }
+
+    public Integer getCodigoCidade() {
+        return codigoCidade;
+    }
+
+    public void setCodigoCidade(Integer codigoCidade) {
+        this.codigoCidade = codigoCidade;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getUf_estado() {
+        return uf_estado;
+    }
+
+    public void setUf_estado(String uf_estado) {
+        this.uf_estado = uf_estado;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    } 
+}
